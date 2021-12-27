8d05d06c9d327ffa266da6927a8f347118a2b9ed
Siu <siuc@protonmail.com>
2021-12-27 12:54:40 -0500

    ad reducers

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const UPDATE_VALUE = "UPDATE_VALUE";
+export const CLEAR = "CLEAR";
+export const SAVE_ITEM = "SAVE_ITEM";
+export const ENTER = "ENTER";
--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,5 +1,21 @@
-const useData = () => {
+import { useEffect } from "react";
+import { UPDATE_VALUE, CLEAR, SAVE_ITEM, ENTER } from "../constants";
 
+const useData = () => {
+  const reducers = {
+    [UPDATE_VALUE]: (state) => {
+      return state;
+    },
+    [CLEAR]: (state) => {
+      return state;
+    },
+    [SAVE_ITEM]: (state) => {
+      return state;
+    },
+    [ENTER]: (state) => {
+      return state;
+    },
+  };
 };
 
 export default useData;
