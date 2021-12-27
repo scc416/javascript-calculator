836c8e667bf96996bad4bd35a5b0755d8d013884
Siu <siuc@protonmail.com>
2021-12-27 14:47:06 -0500

    add new tab to link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
   return (
     <div className="footer">
       Design Inspired by{" "}
-      <a href="https://www.apple.com/shop/product/MQ052LL/A/magic-keyboard-with-numeric-keypad-us-english-silver">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.apple.com/shop/product/MQ052LL/A/magic-keyboard-with-numeric-keypad-us-english-silver"
+      >
         Magic Keyboard
       </a>
     </div>
