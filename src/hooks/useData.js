6769971a92d48b08144e87c49a14d11abe98cf0c
Siu <siuc@protonmail.com>
2021-12-27 13:21:44 -0500

    remake updateValue

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,5 +1,14 @@
 import { useReducer } from "react";
-import { UPDATE_VALUE, CLEAR, ENTER } from "../constants";
+import { UPDATE_VALUE, CLEAR, ENTER, operatorsLst } from "../constants";
+
+const removeZeros = (str) => {
+  let i = str.length;
+  while (i > 1 && str[0] == 0) {
+    str = str.slice(1);
+    i--;
+  }
+  return str;
+}
 
 const useData = () => {
   const reducers = {
@@ -46,7 +55,71 @@ const useData = () => {
   });
 
   const { currentValue, saved, enter } = state;
-  return { currentValue, saved, enter };
+
+  const updateValueAction = (val, previousVal, lastItem, enter) => {
+    let value = val;
+    let newValue = null;
+    let removeLast = false;
+    const b1 = operatorsLst.includes(previousVal);
+    const b2 = operatorsLst.includes(val);
+    if(enter) {
+      if (b2) {
+        newValue = previousVal;
+      }
+      return {
+        type: UPDATE_VALUE,
+        value: value,
+        newValue: newValue,
+        removeLast: removeLast
+      };
+    }
+    if (b1) {
+      if (b2) {
+        if(operatorsLst.includes(lastItem)){
+          removeLast = true;
+        } else if (val == "-") {
+          newValue = previousVal;
+        }
+      } else {
+        if(previousVal == "-" && lastItem == "-") {
+          value = "-" + value;
+        } else {
+          newValue = previousVal;
+        }
+        
+      }
+    } else {
+      if (b2) {
+        newValue = previousVal;
+      } else {
+        if(val == "." && previousVal.split("").includes(".")) {
+          value = previousVal;
+        } else {
+          value = removeZeros(previousVal + val);
+        }
+      }
+    }
+    if(value[0] == ".") {
+      value = "0" + value;
+    }
+    if (newValue) {
+      if(newValue[newValue.length - 1 ] == ".") {
+      newValue += "0";
+      }
+    }
+    
+    return { 
+      type: UPDATE_VALUE,
+      value: value,
+      newValue: newValue,
+      removeLast: removeLast
+    };
+  };
+
+  const updateValue = (val, previousVal, lastItem, enter) => 
+    dispatch(updateValueAction(val, previousVal, lastItem, enter));
+
+  return { currentValue, saved, enter, updateValue };
 };
 
 export default useData;
